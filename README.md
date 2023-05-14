70ee4b94acf5d4547cad2f02458af979f3d66f76
Simen A. W. Olsen <so@bjerk.io>
2023-05-14 15:42:09 +0200

    feat: add dependency ranges

--- README.md
+++ README.md
@@ -4,6 +4,25 @@ Scripts for [microplane](https://github.com/Clever/microplane)
 
 ## Migrate to renovate
 
+```shell
+deno run --allow-write https://esm.sh/gh/simenandre/mp/migrate-to-renovate.ts
 ```
+
+To run it with `microplane`:
+
+```shell
 mp plan -b simenandre/renovate -m 'chore: move to renovate' -- deno run --allow-write https://esm.sh/gh/simenandre/mp/migrate-to-renovate.ts
 ```
+
+## Use dependency ranges
+
+```shell
+deno run --allow-write --allow-read https://esm.sh/gh/simenandre/mp/use-dependency-ranges.ts
+```
+
+To run it with `microplane`:
+
+```shell
+mp plan -b simenandre/dependency-ranges -m 'chore: use dependency ranges' -- deno run --allow-write --allow-read https://esm.sh/gh/simenandre/mp/use-dependency-ranges.ts
+```
+
---
+++ use-dependency-ranges.ts
@@ -0,0 +1,31 @@
+import type { PackageJson } from "https://esm.sh/v120/types-package-json@2.0.39";
+
+const rawPkg = await Deno.readFile("./package.json");
+const pkg = JSON.parse(new TextDecoder().decode(rawPkg)) as PackageJson;
+
+const isRanged = (version: string) =>
+  version.includes("^") || version.includes("~");
+
+if (pkg.dependencies) {
+  for (const [name, version] of Object.entries(pkg.dependencies)) {
+    if (!isRanged(version)) {
+      console.log(`Dependency ${name} is not ranged`);
+      pkg.dependencies[name] = `^${version}`;
+    }
+  }
+}
+
+if (pkg.devDependencies) {
+  for (const [name, version] of Object.entries(pkg.devDependencies)) {
+    if (!isRanged(version)) {
+      console.log(`Dependency ${name} is not ranged`);
+      pkg.devDependencies[name] = `^${version}`;
+    }
+  }
+}
+
+await Deno.writeTextFile(
+  "./package.json",
+  JSON.stringify(pkg, null, 2) + "\n",
+);
+
