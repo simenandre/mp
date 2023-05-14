2d2330dd5824f59bc8c8dbb0b30fb5774ad41a05
Simen A. W. Olsen <so@bjerk.io>
2023-05-14 22:21:18 +0200

    add replace package script

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Scripts for [microplane](https://github.com/Clever/microplane)
 
 ## Migrate to renovate
 
+A script that migrates a repository from [dependabot](https://dependabot.com/) to [renovate](https://renovatebot.com/).
+
 ```shell
 deno run --allow-write https://esm.sh/gh/simenandre/mp/migrate-to-renovate.ts
 ```
@@ -16,6 +18,8 @@ mp plan -b simenandre/renovate -m 'chore: move to renovate' -- deno run --allow-
 
 ## Use dependency ranges
 
+A script that migrates a repository from using exact versions to using dependency ranges.
+
 ```shell
 deno run --allow-write --allow-read https://esm.sh/gh/simenandre/mp/use-dependency-ranges.ts
 ```
@@ -26,3 +30,20 @@ To run it with `microplane`:
 mp plan -b simenandre/dependency-ranges -m 'chore: use dependency ranges' -- deno run --allow-write --allow-read https://esm.sh/gh/simenandre/mp/use-dependency-ranges.ts
 ```
 
+## Replace package
+
+A script that replaces a package with another package.
+
+Make sure to add a version / tag to the new package.
+
+```shell
+deno run --allow-write --allow-read https://esm.sh/gh/simenandre/mp/replace-package.ts old-package new-package@1.0.0
+```
+
+To run it with `microplane`:
+
+```shell
+mp plan -b simenandre/replace-package -m 'chore: replace package' -- deno run --allow-write --allow-read https://esm.sh/gh/simenandre/mp/replace-package.ts old-package new-package@1.0.0
+```
+
+**Note**: You should probably update your lockfile as well!
---
+++ replace-package.ts
@@ -0,0 +1,43 @@
+import type { PackageJson } from "https://esm.sh/v120/types-package-json@2.0.39";
+import { parse } from "https://esm.sh/v121/parse-package-name@1.0.0/deno/parse-package-name.mjs";
+
+const rawPkg = await Deno.readFile("./package.json").catch(() => {
+  // If the file doesn't exist, we exit gracefully
+  console.log("No package.json found");
+  Deno.exit(0);
+});
+const pkg = JSON.parse(new TextDecoder().decode(rawPkg)) as PackageJson;
+
+const oldPackageName = Deno.args[0];
+const newPackage = parse(Deno.args[1]);
+
+if (!oldPackageName || !newPackage.name || !newPackage.version) {
+  console.log({
+    oldPackageName,
+    newPackageName: newPackage.name,
+    newPackageVersion: newPackage.version,
+  })
+  throw new Error(
+    "Usage: deno run --allow-read --allow-write replace-package.ts <old-package-name> <new-package-name>@<version or scope>"
+  );
+}
+
+// Replace in dependencies
+if (pkg.dependencies && pkg.dependencies[oldPackageName]) {
+  console.log(
+    `Replacing ${oldPackageName} with ${newPackage.name} in dependencies`
+  );
+  pkg.dependencies[newPackage.name] = newPackage.version;
+  delete pkg.dependencies[oldPackageName];
+}
+
+// Replace in devDependencies
+if (pkg.devDependencies && pkg.devDependencies[oldPackageName]) {
+  console.log(
+    `Replacing ${oldPackageName} with ${newPackage.name} in devDependencies`
+  );
+  pkg.devDependencies[newPackage.name] = newPackage.version;
+  delete pkg.devDependencies[oldPackageName];
+}
+
+await Deno.writeTextFile("./package.json", JSON.stringify(pkg, null, 2) + "\n");
